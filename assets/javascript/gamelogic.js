77e1732917a253732e198c88c3f44225fed9ff68
Cody Davis <Cody.1337@gmail.com>
2018-10-25 17:19:30 -0500

    updated UI for testing, initial JS variables declaired

---
+++ assets/javascript/gamelogic.js
@@ -0,0 +1,14 @@
+//Declairing Variables##########################################################################
+//Declairing player's stats
+var playerMaxHealth = 0;
+var playerCurrentHealth = 0;
+var playerAttackDice = 0;
+var playerAttackRollNumber = 0;
+var playerAttackBonus = 0;
+//Monster's Stats
+var monsterHealth = 0;
+var monsterAttackDice = 0;
+var monsterAttackRollNumber = 0;
+var monsterAttackBonus = 0;
+//gamecounters and tracking
+var numberMonsterDefeated = 0;
