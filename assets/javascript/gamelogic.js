cf52d176fc2ecc92e0677e56acb85caf49def76b
Cody Davis <Cody.1337@gmail.com>
2018-10-26 10:01:32 -0500

    updated hero portraits, combat logic, and streamlined monsterList popul

--- assets/javascript/gamelogic.js
+++ assets/javascript/gamelogic.js
@@ -10,13 +10,14 @@ var player = {
     damageDiceSide: 0,
     damageNumberRoll: 0,
     damageBonus: 0,
-}
+};
 //Monster's Stats
 var monster = {
     name: "",
     maxHealth: 0,
     currentHealth: 0,
     defense: 0,
+    numberAttacks: 0,
     attackBonus: 0,
     damageDiceSide: 0,
     damageNumberRoll: 0,
@@ -70,18 +71,20 @@ $( document ).ready(function() {
     function monsterSelector() {
 
         //blueprint of all monsters in game
-        var goblin = {name:"test", maxHealth:500, currentHealth:500, defense: 20, attackBonus: 6, diceSides: 6, diceRolls: 3, damageBonus: 6};
+        var monstersList = [
+            {name:"Goblin", maxHealth:76, currentHealth:76, defense: 15, numberAttacks: 1, attackBonus: 4, diceSides: 6, diceRolls: 1, damageBonus: 2},
+            {name:"Orc", maxHealth: 134, currentHealth: 134, defense: 24, numberAttacks: 1, attackBonus: 6, diceSides: 6, diceRolls: 1, damageBonus: 5}
+        ];
 
         //creating a list of monsters so one can be selected randomly.
-        var monstersList = [];
-        monstersList[0] = goblin;
 
-        //Randomly picks a monster from the array and sets global variables
+        //Randomly picks a monster from the array and sets global variables for current monster
         var key= Math.floor(Math.random()*monstersList.length);
         monster.name = monstersList[key].name;
         monster.maxHealth = monstersList[key].maxHealth;
         monster.currentHealth = monstersList[key].currentHealth;
         monster.defense = monstersList[key].defense;
+        monster.numberAttacks = monstersList[key].numberAttacks;
         monster.attackBonus = monstersList[key].attackBonus;
         monster.damageDiceSide = monstersList[key].diceSides;
         monster.damageNumberRoll = monstersList[key].diceRolls;
@@ -108,43 +111,77 @@ $( document ).ready(function() {
         //checking to see if player hits monster or misses
         if (playerAttackRoll > monster.defense) {
 
-            console.log("You hit the monster.");
-                console.log("Player Damage Rolls...");
-                for (var i = 0; i<player.numberAttacks; i++){ // looping through the number of times as player has attacks.
+            damageDealt = 0; //making sure damageDealt is zero.
 
-                    currentAttack = rollDice(player.damageNumberRoll, player.damageDiceSide, player.damageBonus);
-                    damageDealt += currentAttack;
-                }
-                console.log("Monster HP was: "+monster.currentHealth);
-                monster.currentHealth -= damageDealt; //Monsters health looses total ammount of damage as players dealth from all attacks.
-                console.log("Monster Hp after attack is: "+monster.currentHealth);
+            console.log("You hit the monster, player Damage rolls!");
+
+            for (var i = 0; i<player.numberAttacks; i++){ // looping through the number of times as player has attacks.
+
+                currentAttack = rollDice(player.damageNumberRoll, player.damageDiceSide, player.damageBonus);
+                damageDealt += currentAttack;
+            }
+            console.log("Monster HP was: "+monster.currentHealth);
+            monster.currentHealth -= damageDealt; //Monsters health looses total ammount of damage as players dealth from all attacks.
+            console.log("Monster Hp after attack is: "+monster.currentHealth);
         }
         else {
             console.log("you missed the monster!");
         }
         
+
+        //checking to see if the monsters attacks are hitting.
         if (monsterAttackRoll > player.defense) {
+
+            damageDealt = 0; //making sure damageDealt is zero.
+
             console.log("The monster hit you.");
+
+            for (var i = 0; i<monster.numberAttacks; i++){ // looping through the number of times as player has attacks.
+
+                currentAttack = rollDice(player.damageNumberRoll, player.damageDiceSide, player.damageBonus);
+                damageDealt += currentAttack;
+            }
+
+            console.log(player.characterName+"'s HP was: "+player.currentHealth);
+            player.currentHealth -= damageDealt; //Monsters health looses total ammount of damage as players dealth from all attacks.
+            console.log(player.characterName+"'s HP is: "+player.currentHealth);
+        } else {
+
+            console.log("The monster missed you.");
         }
         console.log("end of combat");
 
 
 
-    }
+    };
 
     //handles the math behind a dice roll and returns the total roll
     function rollDice(rolls, sides, bonus) {
         var rollTotal = 0;
         for(var i = 0; i<rolls; i++){
 
-            currentRoll = Math.floor(Math.random()*sides);
+            currentRoll = (Math.floor(Math.random()*sides)+1);
             console.log("Rolled: "+currentRoll);
             rollTotal+=currentRoll;
         }
         rollTotal += bonus;
         console.log(rollTotal);
         return rollTotal;
-    }
+    };
+
+    function checkCombatEndConditions(){
+    
+        if(monster.currentHealth<=0) {
+            console.log("Monster Defeated!!!! *final fantasy victory music");
+            monsterSelector();
+        }
+        else if (player.currentHealth) {
+            console.log("You Died...");
+            //toggle dungeon interface...
+            toggleCharacterSelect();
+        }
+
+    };
 
     //hides the character select interface - only after hero selected
     function toggleCharacterSelect(){
@@ -153,6 +190,7 @@ $( document ).ready(function() {
 
     //shows the adventure interface - called after characterSelect vanishes
 
+    //TODO!!!!
 
     //Function for debugging. prints all player stats to the console.
     function printPlayerStats(){
